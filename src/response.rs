406f31ac7ded3c79fe6f750c3838c5efff236abd
Its <2779546+Its-its@users.noreply.github.com>
2025-07-22 04:15:27 -0700

    Add Generic Responses

--- src/response.rs
+++ src/response.rs
@@ -91,3 +91,93 @@ pub struct PublicSchema {
     pub updated_at: OffsetDateTime,
     pub deleted_at: Option<OffsetDateTime>,
 }
+
+// GENERAL
+
+#[derive(Debug, Serialize, PartialEq, Deserialize, Clone)]
+pub struct ListResponse<T> {
+    pub items: Vec<T>,
+    pub offset: i64,
+    pub limit: i64,
+    pub total: i64,
+}
+
+impl<T> ListResponse<T> {
+    pub fn empty() -> Self {
+        Self {
+            items: Vec::new(),
+            offset: 0,
+            limit: 0,
+            total: 0,
+        }
+    }
+
+    pub fn all(value: Vec<T>) -> Self {
+        Self {
+            offset: 0,
+            limit: value.len() as i64,
+            total: value.len() as i64,
+            items: value,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SingleValue<V>(pub V);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "type", content = "value")]
+pub enum WrappingResponse<V> {
+    Resp(V),
+    Error(ApiErrorResponse),
+}
+
+impl<V> WrappingResponse<V> {
+    pub fn okay(value: V) -> Self {
+        Self::Resp(value)
+    }
+
+    pub fn error<S: Into<String>>(value: S) -> Self {
+        Self::Error(ApiErrorResponse::new(value))
+    }
+
+    pub fn ok(self) -> std::result::Result<V, ApiErrorResponse> {
+        match self {
+            Self::Resp(v) => Ok(v),
+            Self::Error(e) => Err(e),
+        }
+    }
+
+    pub fn as_ok(&self) -> std::result::Result<&V, &ApiErrorResponse> {
+        match self {
+            Self::Resp(v) => Ok(v),
+            Self::Error(e) => Err(e),
+        }
+    }
+
+    pub fn map<N, F: Fn(V) -> N>(self, func: F) -> WrappingResponse<N> {
+        match self {
+            Self::Resp(v) => WrappingResponse::Resp(func(v)),
+            Self::Error(e) => WrappingResponse::Error(e),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, thiserror::Error)]
+pub struct ApiErrorResponse {
+    pub description: String,
+}
+
+impl ApiErrorResponse {
+    pub fn new<S: Into<String>>(value: S) -> Self {
+        Self {
+            description: value.into(),
+        }
+    }
+}
+
+impl std::fmt::Display for ApiErrorResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Api Error Occurred: {}", self.description)
+    }
+}
